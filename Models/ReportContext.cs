1185669f1ae2f8121a778e378ca2dccc3c03508f
blanaid <blanaid.calvert@gmail.com>
2022-03-29 22:25:14 +0100

    First upload of basic report webapi

---
+++ Models/ReportContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ReportWebApi.Models
+{
+    public class ReportContext : DbContext
+    {
+        public ReportContext(DbContextOptions<ReportContext> options)
+    : base(options)
+        {
+        }
+
+        public DbSet<ReportItem> ReportItems { get; set; } = null!;
+    }
+}
